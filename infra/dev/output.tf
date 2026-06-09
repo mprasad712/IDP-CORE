b7e1599199c039d66551e7741240b991bba72cf9
Manas <manasprasad712@gmail.com>
2026-06-10 02:52:25 +0530

    affed pipline

--- infra/dev/output.tf
+++ infra/dev/output.tf
@@ -14,17 +14,12 @@ output "postgres_fqdn" {
   value = azurerm_postgresql_flexible_server.postgres.fqdn
 }
 
-output "redis_hostname" {
-  value = azurerm_redis_cache.redis.hostname
-}
-
-output "redis_ssl_port" {
-  value = azurerm_redis_cache.redis.ssl_port
-}
-
-output "redis_primary_key" {
-  value     = azurerm_redis_cache.redis.primary_access_key
-  sensitive = true
+output "redis_connection_info" {
+  value = {
+    host = azurerm_redis_enterprise_cluster.redis.hostname
+    port = 10000
+    db   = azurerm_redis_enterprise_database.db.name
+  }
 }
 
 output "aks_cluster_name" {
